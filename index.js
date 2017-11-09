c89bc46b04eeefde0b01bd8e463df80aee9d641c
fgfg163 <fgfg163>
2017-11-09 16:03:10 +0800

    no message

--- index.js
+++ index.js
@@ -2,27 +2,33 @@ const http = require('http');
 const parseurl = require('url').parse;
 const zlib = require('zlib');
 const httpProxy = require('http-proxy');
+const program = require('commander')
 
-const proxy = httpProxy.createProxyServer({ ws: true, target: { host: '127.0.0.1', port: 8888 }});
+program
+  .version('0.0.1')
+  .option('-p, --port [number]', 'Server listen port, default 80', '80')
+  .option('-t, --target [host:port]', 'target, default 127.0.0.1:8080', '127.0.0.1:8080')
+  .parse(process.argv)
+
+const target = program.target;
+const tHost = target.replace(/^.*:\/\//, '').replace(/\/.*/, '').replace(/:.*/, '') || '127.0.0.1';
+const tPort = (target.match(/:(\d{1,5})/) || [])[1] || '8080';
+
+const proxy = httpProxy.createProxyServer({ ws: true, target: { host: tHost, port: tPort } });
 
 const s = http.createServer((req, res) => {
-  if (true || req.headers['HOST'] === 'ml.niven.cn') {
-  const opt = parseurl('http://127.0.0.1:8888' + req.url);
+  const opt = parseurl(`http://${tHost}:${tPort}` + req.url);
   opt.headers = req.headers;
   opt.method = req.method;
   const pReq = http.request(opt, pRes => {
     delete pRes.headers['content-length'];
-  pRes.headers['content-encoding'] = 'gzip';
-  res.writeHead(pRes.statusCode, pRes.headers);
-  pRes.pipe(zlib.createGzip()).pipe(res);
-});
+    pRes.headers['content-encoding'] = 'gzip';
+    res.writeHead(pRes.statusCode, pRes.headers);
+    pRes.pipe(zlib.createGzip()).pipe(res);
+  });
   req.pipe(pReq);
-} else {
-  res.writeHead(401);
-  res.end('');
-}
 });
 s.on('upgrade', (req, socket, head) => {
   proxy.ws(req, socket, head);
 });
-s.listen(80);
+s.listen(program.port);
